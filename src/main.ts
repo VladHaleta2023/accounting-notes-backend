39b1c871ead4fb39cfceb58037221a131266d98e
Vlad <125770405+VladHaleta2023@users.noreply.github.com>
2025-06-09 01:55:47 +0200

    Update functions

--- src/main.ts
+++ src/main.ts
@@ -18,13 +18,8 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }));
 
-  const isProd = configService.get<string>('NODE_ENV') === 'production';
-  const allowedOrigin = isProd
-  ? 'https://accounting-notes-frontend.vercel.app'
-  : 'http://localhost:3000';
-
   app.enableCors({
-    origin: allowedOrigin,
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
